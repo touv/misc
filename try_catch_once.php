70b080a9f477739fddd47264374918e6d8ae81b9
Nicolas Thouvenin <nthouvenin@gmail.com>
2010-04-22 16:21:36 +0200

    un peu moins de caractères

--- try_catch_once.php
+++ try_catch_once.php
@@ -1,43 +1,47 @@
 <?php
 // vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4 fdm=marker encoding=utf8 :
-// 
-// try_catch_once, licensed under the MIT license.
-//
-// (c) Nicolas Thouvenin <nthouvenin@gmail.com>
-//
-// For the full copyright and license information, please view the LICENSE
-// file that was distributed with this source code.
-//
-
-// Example 1 : 
-//
-//      $dom = new DomDocument;
-//
-//      $ret = try_func(array($dom,'loadXML'), $xml_string);
-//      if (!$ret) die(catch_error());
-//      
-//      $ret = try_func_array(array($dom, 'relaxNGValidate'), array('schema.rng'));
-//      if (!$ret) die(catch_error());
-//
-//
-// Example 2 : 
-//
-//      $dom = new DomDocument;
-//
-//      $ret = try_method('loadXML', $dom, $xml_string);
-//      if (!$ret) die(catch_error());
-//      
-//      $ret = try_method_array('relaxNGValidate', $dom, array('schema.rng'));
-//      if (!$ret) die(catch_error());
-//
-// Note :
-//
-// You can format in HTML the catch_error output (default in text)
-//
-// echo nl2br(catch_error());
-// echo "<pre>".catch_error()."</pre>";
-// echo catch_error('<br/>');
-//
+/**
+ * try_catch_once, licensed under the MIT license.
+ *
+ * (c) Nicolas Thouvenin <nthouvenin@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * More informations :
+ *  http://github.com/touv/misc/blob/master/try_catch_once.php
+ *
+ *
+ * Example 1 : 
+ *
+ *      $dom = new DomDocument;
+ *
+ *      $ret = try_func(array($dom,'loadXML'), $xml_string);
+ *      if (!$ret) die(catch_error());
+ *      
+ *      $ret = try_func_array(array($dom, 'relaxNGValidate'), array('schema.rng'));
+ *      if (!$ret) die(catch_error());
+ *
+ *
+ * Example 2 : 
+ *
+ *      $dom = new DomDocument;
+ *
+ *      $ret = try_method('loadXML', $dom, $xml_string);
+ *      if (!$ret) die(catch_error());
+ *      
+ *      $ret = try_method_array('relaxNGValidate', $dom, array('schema.rng'));
+ *      if (!$ret) die(catch_error());
+ *
+ * Note :
+ *
+ * You can format in HTML the catch_error output (default in text)
+ *
+ * echo nl2br(catch_error());
+ * echo "<pre>".catch_error()."</pre>";
+ * echo catch_error('<br/>');
+ * 
+ **/
 
 function try_catch_once_error_handler($errno, $errstr, $errfile, $errline, $errcontext, $ret = false)
 {
